51347c62e3ac0b89e4898077a7c8c1895f7752eb
Maria Pires Pacheco <mariapirespac@gmail.com>
2022-07-27 15:05:12 +0200

    Update call_build_sc_model_builder.m

--- call_build_sc_model_builder.m
+++ call_build_sc_model_builder.m
@@ -6,7 +6,7 @@ function [model_composite]=call_build_sc_model_builder(input_model,number_of_clu
 fields_to_keep={'S','rxns', 'mets', 'genes', 'rules', 'grRules', 'b', 'rxnGeneMat','c', 'lb', 'ub', 'rev', 'subSystems'};
 [model_composite]= build_scRNAseq_model(input_model, number_of_cluster,run_toy_model,fields_to_keep);
 %% sanity check
-[model_composite]=fixIrr(model_composite);
+[model_composite]=fixIrr_rFASTCORMICS(model_composite);
 % A=fastcc(model_composite, 1e-4);
 % if numel(A)< numel(model_composite.rxns)
 %     'composite_model is inconsistent';
