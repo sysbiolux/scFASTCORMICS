909d6ac99f06b95e9c625cb5de2b1e0e06ad271c
Leonie THOMAS <leonie.thomas@uni.lu>
2025-03-05 10:30:28 +0100

    adding a toy model to the script so that I can go through the rules, and check that they are really correct

--- RunningExample_Keratinocytes2024/investigate_loop_GPR_rule.m
+++ RunningExample_Keratinocytes2024/investigate_loop_GPR_rule.m
@@ -27,4 +27,38 @@ toc
 sum(mappinga ~= mappingb)
 
 
+%% toy data - to check if the matrix wise computation of the GPR are correct
+
+data = [ 1 0 5 8;
+         0 3 0 8;
+         0 4 6 8;
+         1 5 2 7; 
+         1 6 3 1];
+
+correct_results = [ 0 3 5 8;
+                    1 4 2 1;
+                    1 4 6 8];
+
+rules = ["( x(1) | x(2) ) & x(3)";...
+         "( x(1) | x(2) | x(3) ) & x(4) & x(5)";...
+         "( x(4) & x(1) ) | ( x(2) | x(3) )" ]
+
+disp("changed rules:")
+rules = regexprep(rules,'x\(([0-9]*)\)','x($1,:)')'
+mapping = zeros(numel(rules), size(data,2));
+
+for k=1:numel(rules)
+            mapping(k,:)= GPRrulesMapper_rFASTCORMICS(cell2mat(rules(k)),...
+                                                          data);
+end
+
+sum(mapping ~= correct_results)
+
+
+
+
+
+
+
+
 
